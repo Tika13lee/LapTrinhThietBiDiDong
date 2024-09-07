499d97a2240ee216226f9836f48901d91295c03f
Tika13lee <thuykieu130303@gmail.com>
2024-09-07 22:19:32 +0700

    react-native login2a

--- tuan_03/App.js
+++ tuan_03/App.js
@@ -6,7 +6,8 @@ import Login1b from "./src/ui/Login1b";
 import Login1c from "./src/ui/Login1c";
 import Login1d from "./src/ui/Login1d";
 import Login1e from "./src/ui/Login1e";
+import Login2a from "./src/ui/Login2a";
 
 export default function App() {
-  return <Login1e />;
+  return <Login2a />;
 }
---
+++ tuan_03/src/ui/Login2a.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { View, Text, TouchableOpacity, TextInput, Image } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+function Login1e() {
+  return (
+    <LinearGradient
+      colors={["#FBCB00", "#BF9A00"]}
+      style={{ flex: 100, justifyContent: "center", alignItems: "center" }}
+    >
+      <View
+        style={{
+          flex: 25,
+          alignItems: "flex-start",
+          justifyContent: "center",
+          width: 350,
+        }}
+      >
+        <Text style={{ fontSize: 30, fontWeight: "bold" }}>LOGIN</Text>
+      </View>
+      <View
+        style={{
+          flex: 35,
+          width: 350,
+        }}
+      >
+        <View
+          style={{
+            height: 50,
+            backgroundColor: "#C4C4C44D",
+            paddingHorizontal: 13,
+            borderColor: "#F2F2F2",
+            borderWidth: 1,
+            marginBottom: 30,
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Image source={require("../../assets/avatar_user.png")} />
+          <View style={{ width: 10 }} />
+          <TextInput style={{ fontSize: 20 }}>
+            <Text>Name</Text>
+          </TextInput>
+        </View>
+        <View
+          style={{
+            backgroundColor: "#C4C4C44D",
+            flexDirection: "row",
+            height: 50,
+            alignItems: "center",
+            justifyContent: "space-between",
+            paddingHorizontal: 13,
+            borderColor: "#F2F2F2",
+            borderWidth: 1,
+          }}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <Image source={require("../../assets/lock1.png")} />
+            <View style={{ width: 10 }} />
+            <TextInput style={{ fontSize: 20 }}>
+              <Text>Password</Text>
+            </TextInput>
+          </View>
+          <Image source={require("../../assets/eye1.png")} />
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 40,
+          width: 350,
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#060000",
+            width: 350,
+            height: 50,
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 20,
+            borderRadius: 2,
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 25, fontWeight: "bold" }}>
+            LOGIN
+          </Text>
+        </TouchableOpacity>
+        <Text style={{ fontSize: 25, fontWeight: "bold" }}>CREATE ACCOUNT</Text>
+      </View>
+    </LinearGradient>
+  );
+}
+
+export default Login1e;
