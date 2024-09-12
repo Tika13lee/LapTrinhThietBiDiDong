352bddb9912f0e20db6525469df388539fde73da
Tika13lee <thuykieu130303@gmail.com>
2024-09-12 14:10:39 +0700

    react-native lab02 review

--- tuan_04/App.js
+++ tuan_04/App.js
@@ -2,7 +2,8 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import TikiOk from "./src/TikiOk";
 import GeneratePassword from "./src/GeneratePassword";
+import Review from "./src/Review";
 
 export default function App() {
-  return <GeneratePassword />;
+  return <Review />;
 }
